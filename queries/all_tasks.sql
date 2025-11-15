02f24b2a1f1cfa332181d6fc9a68c2786064190e
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-11-15 14:17:17 +0630

    Add stored procedures: GetMaxQuantity, GetOrderDetail, CancelOrder

--- queries/all_tasks.sql
+++ queries/all_tasks.sql
@@ -36,3 +36,28 @@ FROM Menu
 WHERE MenuID = ANY (
     SELECT MenuID FROM Orders WHERE Quantity > 2
 );
+
+-- 4: GetMaxQuantity
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS GetMaxQuantity;
+CREATE PROCEDURE GetMaxQuantity()
+BEGIN
+    SELECT MAX(Quantity) AS MaxOrderedQuantity FROM Orders;
+END //
+DELIMITER ;
+
+-- 5: GetOrderDetail Prepared Statement
+
+PREPARE GetOrderDetail FROM 
+'SELECT OrderID, Quantity, TotalCost FROM Orders WHERE CustomerID = ?';
+
+-- 6: CancelOrder
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS CancelOrder;
+CREATE PROCEDURE CancelOrder(IN p_order_id INT)
+BEGIN
+    DELETE FROM Orders WHERE OrderID = p_order_id;
+END //
+DELIMITER ;
